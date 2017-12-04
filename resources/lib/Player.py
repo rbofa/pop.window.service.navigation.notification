9f400a8246aee1c24682ea546a304bc2b236a5d8
rbofa <rameshkp@gmail.com>
2017-12-04 18:51:05 +0000

    Player file service class for playback monitoring.

--- resources/lib/Player.py
+++ resources/lib/Player.py
@@ -18,6 +18,20 @@ class Player(xbmc.Player):
 
     xbmcplayer = xbmc.Player()
     clientInfo = ClientInformation()
+    addonName = clientInfo.getAddonName()
+    addonId = clientInfo.getAddonId()
+    addon = xbmcaddon.Addon(id=addonId)
+
+    logLevel = 0
+    currenttvshowid = None
+    currentepisodeid = None
+    playedinarow = 1
+
+    fields_base = '"dateadded", "file", "lastplayed","plot", "title", "art", "playcount",'
+    fields_file = fields_base + '"streamdetails", "director", "resume", "runtime",'
+    fields_tvshows = fields_base + '"sorttitle", "mpaa", "premiered", "year", "episode", "watchedepisodes", "votes", "rating", "studio", "season", "genre", "episodeguide", "tag", "originaltitle", "imdbnumber"'
+    fields_episodes = fields_file + '"cast", "productioncode", "rating", "votes", "episode", "showtitle", "tvshowid", "season", "firstaired", "writer", "originaltitle"'
+    postplaywindow = None
 
     def __init__(self, *args):
         self.__dict__ = self._shared_state
