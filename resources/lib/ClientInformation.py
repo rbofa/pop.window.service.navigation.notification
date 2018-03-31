9a3a65f056dc39591d46fcf25b1e012d29ae281b
rbofa <rameshkp@gmail.com>
2018-03-31 09:59:25 +0100

    Client Information to get play modes

---
+++ resources/lib/ClientInformation.py
@@ -0,0 +1,39 @@
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcvfs
+
+import os
+from uuid import uuid4 as uuid4
+
+import Utils as utils
+
+
+class ClientInformation():
+    def __init__(self):
+        addonId = self.getAddonId()
+        self.addon = xbmcaddon.Addon(id=addonId)
+
+        self.className = self.__class__.__name__
+        self.addonName = self.getAddonName()
+
+    def logMsg(self, msg, lvl=1):
+        utils.logMsg("%s %s" % (self.addonName, self.className), str(msg), int(lvl))
+
+    def getAddonId(self):
+        # To use when declaring xbmcaddon.Addon(id=addonId)
+        return "pop.window.service.navigation.notification"
+
+    def getAddonName(self):
+        # Useful for logging
+        return self.addon.getAddonInfo('name').upper()
+
+    def getPlayMode(self):
+        if self.addon.getSetting("showPostPlay") == "true":
+            return "PostPlay"
+        else:
+            return "PrePlay"
+
+    def getVersion(self):
+        return self.addon.getAddonInfo('version')
+
