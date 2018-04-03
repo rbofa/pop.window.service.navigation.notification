d35431e04ed705a3878abd500fe64beb6bb3f5a5
rbofa <rameshkp@gmail.com>
2018-04-03 19:48:24 +0100

    Next available video to play

---
+++ resources/lib/NextUpInfo.py
@@ -0,0 +1,19 @@
+import xbmc
+import xbmcgui
+from platform import machine
+
+ACTION_PLAYER_STOP = 13
+OS_MACHINE = machine()
+
+
+class NextUpInfo(xbmcgui.WindowXMLDialog):
+    item = None
+    cancel = False
+    watchnow = False
+
+    def __init__(self, *args, **kwargs):
+        if OS_MACHINE[0:5] == 'armv7':
+            xbmcgui.WindowXMLDialog.__init__(self)
+        else:
+            xbmcgui.WindowXMLDialog.__init__(self, *args, **kwargs)
+
