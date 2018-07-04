de702f5d61b96b7fb3566e10d04f6a0a1f0e9237
rp <rameshkp@gmail.com>
2018-07-04 19:44:08 +0100

    Post play suggestion to choose next vedio.

---
+++ resources/lib/PostPlayInfo.py
@@ -0,0 +1,14 @@
+import xbmc
+import xbmcaddon
+import xbmcgui
+from platform import machine
+import time
+import threading
+
+ACTION_PLAYER_STOP = 13
+OS_MACHINE = machine()
+
+PASSOUT_PROTECTION_DURATION_SECONDS = 7200
+PASSOUT_LAST_VIDEO_DURATION_MILLIS = 1200000
+
+class PostPlayInfo(xbmcgui.WindowXML):
