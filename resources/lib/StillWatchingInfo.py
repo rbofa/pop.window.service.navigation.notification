44fe514ff7b8ac8cfc8d8fb26031a43ad1dd4872
ramesh kp <rameshkp@gmail.com>
2020-01-20 18:36:16 +0000

    Still watching paused to resume status

--- resources/lib/StillWatchingInfo.py
+++ resources/lib/StillWatchingInfo.py
@@ -7,3 +7,8 @@ class StillWatchingInfo(xbmcgui.WindowXMLDialog):
     item = None
     cancel = False
     stillwatching = False
+
+    def __init__(self, *args, **kwargs):
+        self.action_exitkeys_id = [10, 13]
+        if OS_MACHINE[0:5] == 'armv7':
+            xbmcgui.WindowXMLDialog.__init__(self)
