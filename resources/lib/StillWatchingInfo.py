98f5ffbccc697393d4f7037e0def2257ea83f57c
ramesh kp <rameshkp@gmail.com>
2020-03-21 13:09:07 +0000

    Still watching paused to resume status

--- resources/lib/StillWatchingInfo.py
+++ resources/lib/StillWatchingInfo.py
@@ -3,6 +3,11 @@ from platform import machine
 import xbmc
 import xbmcgui
 
+
+ACTION_PLAYER_STOP = 13
+OS_MACHINE = machine()
+
+
 class StillWatchingInfo(xbmcgui.WindowXMLDialog):
     item = None
     cancel = False
@@ -22,7 +27,7 @@ class StillWatchingInfo(xbmcgui.WindowXMLDialog):
         fanartimage = self.item['art'].get('tvshow.fanart', '')
         clearartimage = self.item['art'].get('tvshow.clearart', '')
         name = self.item['label']
-        rating = str(round(float(self.item['rating']), 1))
+        rating = str(round(float(self.item['rating']),1))
         year = self.item['firstaired']
         overview = self.item['plot']
         season = self.item['season']
@@ -51,17 +56,17 @@ class StillWatchingInfo(xbmcgui.WindowXMLDialog):
 
         try:
             landscapeControl = self.getControl(4005)
-        if landscapeControl is not None:
-            self.getControl(4005).setImage(landscape)
+            if landscapeControl is not None:
+                self.getControl(4005).setImage(landscape)
         except:
-              pass
+            pass
 
         try:
             fanartControl = self.getControl(4007)
             if fanartControl is not None:
                 fanartControl.setImage(fanartimage)
         except:
-        pass
+            pass
 
         try:
             seasonControl = self.getControl(4008)
@@ -107,17 +112,48 @@ class StillWatchingInfo(xbmcgui.WindowXMLDialog):
         except:
             pass
 
-        def setItem(self, item):
-            self.item = item
+    def setItem(self, item):
+        self.item = item
 
-        def setCancel(self, cancel):
-            self.cancel = cancel
+    def setCancel(self, cancel):
+        self.cancel = cancel
 
-        def isCancel(self):
-            return self.cancel
+    def isCancel(self):
+        return self.cancel
 
-        def setStillWatching(self, stillwatching):
-            self.stillwatching = stillwatching
+    def setStillWatching(self, stillwatching):
+        self.stillwatching = stillwatching
 
-        def isStillWatching(self):
-            return self.stillwatching
+    def isStillWatching(self):
+        return self.stillwatching
+
+    def onFocus(self, controlId):
+        pass
+
+    def doAction(self):
+        pass
+
+    def closeDialog(self):
+        self.close()
+
+    def onClick(self, controlID):
+
+        xbmc.log("still watching info onclick: " + str(controlID))
+
+        if controlID == 4012:
+            # still watching
+            self.setStillWatching(True)
+            self.close()
+
+        elif controlID == 4013:
+            # cancel
+            self.setCancel(True)
+            self.close()
+
+        pass
+
+    def onAction(self, action):
+        xbmc.log("still watching info action: " + str(action.getId()))
+        if action == ACTION_PLAYER_STOP:
+            self.close()
+
