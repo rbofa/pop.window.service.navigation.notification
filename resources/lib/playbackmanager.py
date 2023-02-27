d9e9220811a8afbe79d2740acec0c8efddcd6851
Ramesh Pydi <rameshbofa@gmail.com>
2023-02-27 19:40:17 +0000

    RP: SNI - 113 - Play back manager to manage list.

---
+++ resources/lib/playbackmanager.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# GNU General Public License v2.0 (see COPYING or https://www.gnu.org/licenses/gpl-2.0.txt)
+
+from __future__ import absolute_import, division, unicode_literals
+from xbmc import sleep
+from api import Api
+from demo import DemoOverlay
+from player import Player
+from playitem import PlayItem
+from state import State
+from stillwatching import StillWatching
+from upnext import UpNext
+from utils import addon_path, calculate_progress_steps, clear_property, event, get_setting_bool, get_setting_int, log as ulog, set_property
+
+
+class PlaybackManager:
+    _shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self._shared_state
+        self.api = Api()
+        self.play_item = PlayItem()
+        self.state = State()
+        self.player = Player()
+        self.demo = DemoOverlay(12005)
