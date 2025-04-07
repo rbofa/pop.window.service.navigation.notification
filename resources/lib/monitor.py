76830bb4571aa1132f04d4ab5e6ecb3555fb213b
Ramesh Pydi <33283442+rbofa@users.noreply.github.com>
2025-04-07 16:11:46 +0100

    Update monitor.py

--- resources/lib/monitor.py
+++ resources/lib/monitor.py
@@ -97,7 +97,7 @@ class UpNextMonitor(Monitor):
             self.player.set_last_file(from_unicode(current_file))
             self.log('Show notification as episode (of length %d secs) ends in %d secs' % (total_time, notification_time), 2)
             self.playback_manager.launch_up_next()
-            self.log('Up Next style autoplay succeeded', 2)
+            self.log('Up Next style autoplay succeeded.', 2)
             self.player.disable_tracking()
 
             self.log('Service stopped', 0)
